42fd22a0c8ee8e953b93cf4f9e5956a69872c8e3
Ubuntu <ubuntu@ip-172-31-27-213.ec2.internal>
2025-03-12 18:53:37 +0000

    Authenticate Snyk in the CI/CD pipeline

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-    console.log(`App running on http://localhost:${port}`);
+app.listen(3000, '0.0.0.0', () => {
+    console.log('App running on http://localhost:3000');
 });
+
